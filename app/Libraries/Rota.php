bdbc7c00fa7cf2747b8d7ad2a90154a3cdd0f6ff
Claudinei Borges Leal <cborgesleal@gmail.com>
2021-05-30 20:31:28 -0300

    Criado o metodo url da classe Rota

--- app/Libraries/Rota.php
+++ app/Libraries/Rota.php
@@ -2,7 +2,24 @@
 
 class Rota
 {
-    public function __construct() {
-        echo 'Nossa primeira classe';
+    public function __construct() 
+    {
+        $this->url();
+
+        echo '<pre>'; print_r($this->url()); exit;
+    }
+
+    private function url()
+    {
+        if (isset($_GET['url'])):
+            # limpar a URL
+            $url = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_URL);
+            # remover espaços e '/'
+            $url = trim(rtrim($url, '/'));
+            # transformar a string url em um array
+            $url = explode('/', $url);
+            # retorna a url
+            return $url;
+        endif;
     }
 }
