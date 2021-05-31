68724f55c988355c0f2c71d499776efdc2be5255
Claudinei Borges Leal <cborgesleal@gmail.com>
2021-05-30 21:11:24 -0300

    Classe Rota Concluida

--- app/Libraries/Rota.php
+++ app/Libraries/Rota.php
@@ -2,11 +2,40 @@
 
 class Rota
 {
+    private $controlador = 'Paginas';
+    private $metodo = 'index';
+    private $parametros = [];
+
     public function __construct() 
     {
-        $this->url();
+        $url = $this->url() ? $this->url() : [0];        
 
-        echo '<pre>'; print_r($this->url()); exit;
+        # controlador
+        if (isset($url)):
+            if (file_exists('../app/Controllers/'.ucwords($url[0]).'.php')):
+                $this->controlador = ucwords($url[0]);
+                unset($url[0]);
+            endif;
+        endif;
+
+        # requerer o controlador
+        require_once '../app/Controllers/'.$this->controlador.'.php';
+        # instanciar a classe controlador
+        $this->controlador = new $this->controlador;
+
+        # metodo       
+        if (isset($url[1])):
+            if (method_exists($this->controlador, $url[1])):
+                $this->metodo = $url[1];
+                unset($url[1]);
+            endif;
+        endif;
+
+        # parametros       
+        $this->parametros = $url ? array_values($url) : [];
+        call_user_func_array([$this->controlador, $this->metodo], $this->parametros);
+
+        // echo '<pre>'; print_r($this); exit;
     }
 
     private function url()
