454fb0cbbc681d5a244bf6a1ba7dcca7d5310cf6
Claudinei Borges Leal <cborgesleal@gmail.com>
2021-06-03 20:05:17 -0300

    Restringindo Acesso ao Controlador Posts

--- app/Controllers/Posts.php
+++ app/Controllers/Posts.php
@@ -2,6 +2,17 @@
 
 class Posts extends Controller
 {
+    //===============================================================
+    public function __construct()
+    {
+        # verificar se o usuario está logado (na sessão)
+        if (!isset($_SESSION['usuario_id'])):
+            # manda para a view login
+            Url::redirecionar('usuarios/login');
+        endif;
+    }
+
+    //===============================================================
     public function index()
     {
         # mandar para a view index de posts
